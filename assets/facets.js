0ee2c01cc8197c5e474071f8efcb671689df4035
RiisedDK <kr@riised.dk>
2024-07-03 05:22:35 -0700

    After Optimization
    After Optimization

--- assets/facets.js
+++ assets/facets.js
@@ -22,6 +22,14 @@ class FacetsToggle {
   }
 }
 
+function debounce(func, wait) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+}
 
 class FacetFiltersForm extends HTMLElement {
   constructor() {
